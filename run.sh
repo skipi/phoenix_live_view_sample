195793dd067d9717760ab31fa364c5f11c9a3d43
Nick Kutryj <nick@tokagen.com>
2019-07-02 10:47:10 +0200

    Fix

--- run.sh
+++ run.sh
@@ -3,9 +3,14 @@ set -ex
 
 mix do deps.get, compile
 
+cd assets && npm install && cd ..
+
 chown -R $USR deps 
 chown -R $USR _build
+chown -R $USR assets/node_modules
 chgrp -R $GRP deps
 chgrp -R $GRP _build
+chgrp -R $GRP assets/node_modules
+
 
 mix phx.server
